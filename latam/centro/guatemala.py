b0380106918b2bf636de0bab42b58379261cb80d
Sergio Sanchez <ssanchez@alluma.org>
2020-10-19 16:29:03 -0700

    agrega Huehuetenango x2

--- latam/centro/guatemala.py
+++ latam/centro/guatemala.py
@@ -52,9 +52,9 @@ PE = Subdivision(
     abrev="PET",
     nombre_pronunciacion_local="pet.'en",
     codigo="PE",
-    codigo_numerico=1,
+    codigo_numerico=17,
     capital=ciudad_flores,
-    capital_horario=timezone("America/Mexico_City"),
+    capital_horario=timezone("America/Guatemala"),
     capital_latlong=ciudad_flores.latlong,
     es_contigua=True,
     es_isla=False,
@@ -63,4 +63,33 @@ PE = Subdivision(
     ),  # https://guatehistoria.com/datos-historicos-del-peten/
     husos_horarios=[timezone("America/Guatemala")],
     ciudades_grandes={"Ciudad Flores": ciudad_flores},
+)
+
+huehuetenango = Ciudad(
+    nombre="Huehuetenango",
+    nombre_comun="Huehuetenango",
+    otros_nombres=["Xinabajul"],
+    abrev="HUE",
+    nombre_pronunciacion_local="we.we.te.'nan.ɡo",
+    fecha_de_fundacion=dt.date(1866, 11, 23),
+    huso_horario=timezone("America/Guatemala"),
+    latlong=(15.31, -91.48),
+)
+
+HU = Subdivision(
+    nombre="Huehuetenango",
+    nombre_comun="Huehuetenango",
+    nombres_nativos={"Mam": "Xinabajul"},
+    abrev="HUE",
+    nombre_pronunciacion_local="we.we.te.'nan.ɡo",
+    codigo="HU",
+    codigo_numerico=13,
+    capital=huehuetenango,
+    capital_horario=timezone("America/Guatemala"),
+    capital_latlong=huehuetenango.latlong,
+    es_contigua=True,
+    es_isla=False,
+    fecha_de_fundacion=dt.date(1866, 5, 8),
+    husos_horarios=[timezone("America/Guatemala")],
+    ciudades_grandes={"Huehuetenango": huehuetenango},
 )
